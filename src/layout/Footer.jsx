93e5faf1d2945d9bad9eb96cda742ddc0b220ef9
stavbers <reddevweb@gmail.com>
2023-05-14 23:49:50 +0300

    some fix

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,16 +1,14 @@
 function Footer() {
   const date = new Date();
-  const [month, day, year] = [
-    date.getMonth(),
-    date.getDay(),
+  const [year] = [
     date.getFullYear(),
   ]
   return <footer className="page-footer">
   <div className="footer-copyright">
     <div className="container">
     {/* © {new Date().getFullYear()} Copyright Text */}
-    © {month}/{day}/{year} Copyright Text
-    <a className="grey-text text-lighten-4 right" href="!#">More Links</a>
+    ©{year} Copyright Text
+    <a className="grey-text text-lighten-4 right" target='_blank' rel="noreferrer" href="https://github.com/stavbers">GitHub</a>
     </div>
   </div>
 </footer>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,11 +1,11 @@
 function Header() {
 return <nav>
   <div className="nav-wrapper  red darken-4">
-    <a href="!#" className="brand-logo right">Logo</a>
+    <a href="!#" className="brand-logo right">MovPlay</a>
     <ul id="nav-mobile" className="left hide-on-med-and-down">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">JavaScript</a></li>
+      <li><a href="sass.html">Films</a></li>
+      <li><a href="badges.html">Movie</a></li>
+      <li><a href="collapsible.html">Serials</a></li>
     </ul>
   </div>
 </nav>
